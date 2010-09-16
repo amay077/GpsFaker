b30bfae6c907e3d86cbc92053a81a7e29691753d
amay077 <okuokuoku@gmail.com>
2010-09-16 18:32:28 +0900

    ファーストリリース

--- src/amay077/android/samples/GpsFaker/GpsFaker.java
+++ src/amay077/android/samples/GpsFaker/GpsFaker.java
@@ -1,13 +1,162 @@
 package amay077.android.samples.GpsFaker;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class GpsFaker extends Activity {
-    /** Called when the activity is first created. */
+
+	// fields -----------------------------------------------------------------
+	private GpsSignalService m_gpsService = null;
+	private Context m_context = this;
+	private ServiceConnection m_serviceConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			m_gpsService = ((GpsSignalService.GpsSignalBinder)service).getService();
+
+			IntentFilter filter = new IntentFilter(GpsSignalService.CHANGE_LOCATION);
+			registerReceiver(m_receiver, filter);
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName className) {
+			m_gpsService = null;
+		}
+	};
+
+	private BroadcastReceiver m_receiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			/*
+			double lat = intent.getDoubleExtra("Latitude", 0d);
+			double lng = intent.getDoubleExtra("Longitude", 0d);
+			double time = intent.getLongExtra("Time", 0);
+
+			String mes = String.format("GPSの位置が変更されました。- lat:%f, long:%f, time:%d", lat, lng, time);
+
+			showToast(mes);
+*/
+		}
+	};
+
+	// ctor -------------------------------------------------------------------
+
+	// setter/getter ----------------------------------------------------------
+
+	// overrides --------------------------------------------------------------
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // サービス開始
+        Button btn = (Button)findViewById(R.id.ButtonStartService);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (m_gpsService != null) return;
+				startService();
+				showToast("サービスを開始しました。");
+			}
+		});
+
+        // サービス停止
+        btn = (Button)findViewById(R.id.ButtonStopService);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (m_gpsService == null) return;
+				stopService();
+				showToast("サービスを停止しました。");
+			}
+		});
+
+        // ログファイル選択
+        btn = (Button)findViewById(R.id.ButtonSelectGpsLog);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+			   	if (m_gpsService == null) return;
+				m_gpsService.init("/sdcard/gps.log");
+				showToast("GPSログファイルを設定しました。");
+			}
+		});
+
+        // 再生
+        btn = (Button)findViewById(R.id.ButtonPlay);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+			   	if (m_gpsService == null) return;
+				m_gpsService.play();
+				showToast("GPSログ再生を開始しました。");
+			}
+		});
+
+        // 一時停止
+        btn = (Button)findViewById(R.id.ButtonPause);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+			   	if (m_gpsService == null) return;
+				m_gpsService.pause();
+				showToast("GPSログ再生を一時停止しました。Play で再開します。");
+			}
+		});
+
+        // 停止
+        btn = (Button)findViewById(R.id.ButtonStop);
+        btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+			   	if (m_gpsService == null) return;
+				m_gpsService.stop();
+				showToast("GPSログ再生を停止しました。Play で最初から再生します。");
+			}
+		});
+
+		// 既に起動しているかもしれないサービスに接続
+        Intent intent = new Intent(m_context, GpsSignalService.class);
+		bindService(intent, m_serviceConnection, 0);
     }
+
+	// public methods ---------------------------------------------------------
+
+	// private methods --------------------------------------------------------
+
+    private void startService() {
+		// サービスを開始
+		Intent intent = new Intent(m_context, GpsSignalService.class);
+		startService(intent);
+
+		// サービスにバインド
+		bindService(intent, m_serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void stopService() {
+    	if (m_gpsService == null) return;
+
+		unbindService(m_serviceConnection); // バインド解除
+		unregisterReceiver(m_receiver); // 登録解除
+		m_gpsService.stopSelf();
+    }
+
+    private void showToast(String mes) {
+    	Toast.makeText(GpsFaker.this, mes, Toast.LENGTH_SHORT).show();
+    }
+
+	// sub classes ------------------------------------------------------------
+
 }
--- src/amay077/android/samples/GpsFaker/GpsSignalService.java
+++ src/amay077/android/samples/GpsFaker/GpsSignalService.java
@@ -1,10 +1,26 @@
 /**
- * 
+ *
  */
 package amay077.android.samples.GpsFaker;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Binder;
 import android.os.IBinder;
 
 /**
@@ -13,13 +29,288 @@ import android.os.IBinder;
  */
 public class GpsSignalService extends Service {
 
-	/* (非 Javadoc)
-	 * @see android.app.Service#onBind(android.content.Intent)
-	 */
+/*	// public fields ----------------------------------------------------------
+	public static final String ACTION = "Gps Faker Signal";
+*/
+	// fields -----------------------------------------------------------------
+	public static final String CHANGE_LOCATION = "GpsFaker ChangeLocation";
+	private final String PROVIDER_NAME = LocationManager.GPS_PROVIDER;
+
+	private Timer m_timer = new Timer();
+	private LocationManager m_locMan = null;
+	private int m_interval = 1000;
+	private BufferedReader m_gpsLogReader = null;
+
+	// 通知領域
+	private NotificationManager m_notificationManager = null;
+	private Notification m_notification = null;
+
+	// ctor -------------------------------------------------------------------
+	// - none -
+
+	// setter/getter ----------------------------------------------------------
+
+	public int getInterval() {
+		return m_interval;
+	}
+	public void setInterval(int mInterval) {
+		m_interval = mInterval;
+	}
+
+	// overrides --------------------------------------------------------------
+
 	@Override
 	public IBinder onBind(Intent arg0) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return new GpsSignalBinder();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		stop();
+	}
+
+	// public methods ---------------------------------------------------------
+
+	public boolean init(String path) {
+		// GPSログ読み込み
+		try {
+			m_locMan = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+			m_locMan.addTestProvider(PROVIDER_NAME,
+	        		false, //requiresNetwork,
+	        		true,  //requiresSatellite,
+	        		false, //requiresCell,
+	        		false, //hasMonetaryCost,
+	        		true,  //supportsAltitude,
+	        		true,  //supportsSpeed,
+	        		false, //supportsBearing,
+	        		0,     //powerRequirement,
+	        		1);    //accuracy)
+
+			m_locMan.setTestProviderEnabled(PROVIDER_NAME, true);
+			m_locMan.setTestProviderStatus(PROVIDER_NAME,
+					LocationProvider.AVAILABLE, null, System.currentTimeMillis());
+
+
+
+			if (m_gpsLogReader != null) {
+				m_gpsLogReader.close();
+			}
+
+			m_gpsLogReader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+
+			// 通知領域の初期化
+			initNotify();
+
+		} catch (FileNotFoundException e) {
+			return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+
+	public void unInit() {
+
+		try {
+			stopTimer();
+
+			if (m_gpsLogReader != null) {
+				m_gpsLogReader.close();
+				m_gpsLogReader = null;
+			}
+
+			if (m_locMan != null) {
+				m_locMan.setTestProviderStatus(PROVIDER_NAME,
+						LocationProvider.OUT_OF_SERVICE, null, System.currentTimeMillis());
+				m_locMan.setTestProviderEnabled(PROVIDER_NAME, false);
+				m_locMan.removeTestProvider(PROVIDER_NAME);
+				m_locMan = null;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void play() {
+		if (m_locMan == null) return;
+
+		stopTimer();
+
+		// 通知開始
+		startNotify();
+
+		m_timer = new Timer(false);
+		TimerTask timerTask = new GpsSignalTask(m_locMan, m_gpsLogReader);
+		m_timer.schedule(timerTask, 0, m_interval);
+	}
+
+	public void pause() {
+		if (m_locMan == null) return;
+
+		stopTimer();
+	}
+
+	public void stop() {
+		if (m_locMan == null) return;
+
+		stopTimer();
+
+		if (m_gpsLogReader != null) {
+			try {
+				m_gpsLogReader.reset();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// 通知停止
+		stopNotify();
+	}
+
+	// private methods --------------------------------------------------------
+	private void stopTimer() {
+		if (m_timer != null) {
+			m_timer.cancel();
+			m_timer.purge();
+		}
+	}
+
+	/***
+	 * 通知のためのオブジェクトの初期化を行う
+	 */
+	private void initNotify() {
+		m_notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		if (m_notificationManager == null) {
+			return;
+		}
+
+		m_notification = new Notification(
+				R.drawable.icon,
+				"GPSログの再生を開始しました。",
+				System.currentTimeMillis());
+
+		// intentの設定
+		PendingIntent contentIntent =
+				PendingIntent.getActivity(this, 0, new Intent(this, GpsFaker.class), 0);
+
+		m_notification.setLatestEventInfo(
+				getApplicationContext(),
+				getText(R.string.app_name),
+				"GPSログを再生しています。",
+				contentIntent);
+
+		// 「実行中」の領域に表示する
+		m_notification.flags = Notification.FLAG_ONGOING_EVENT;
+	}
+
+	/***
+	 * 通知領域への通知を開始する
+	 */
+	private void startNotify() {
+		if (m_notificationManager != null) {
+			m_notificationManager.notify(R.string.app_name, m_notification);
+		}
+	}
+
+	/***
+	 * 通知領域への通知を終了する
+	 */
+	private void stopNotify() {
+		if (m_notificationManager != null) {
+			m_notificationManager.cancel(R.string.app_name);
+		}
+	}
+
+	// Sub Classes ------------------------------------------------------------
+	class GpsSignalTask extends TimerTask {
+		// fields -----------------------------------------------------------------
+		private Location m_location = null;
+		private LocationManager m_locMan = null;
+		private BufferedReader m_gpsLogReader = null;
+
+		// ctor -------------------------------------------------------------------
+		public GpsSignalTask(LocationManager locMan, BufferedReader gpsLogReader) {
+			super();
+
+			this.m_locMan = locMan;
+			this.m_gpsLogReader = gpsLogReader;
+		}
+
+		// setter/getter ----------------------------------------------------------
+		// - none -
+
+		// overrides --------------------------------------------------------------
+		@Override
+		public void run() {
+			try {
+
+				// ファイルから１行よむ
+				String line = m_gpsLogReader.readLine();
+				if (line == null) {
+					return;
+				}
+
+				// カンマで分割（経度,緯度,高度）
+				String[] buf = line.split(",");
+				Location loc = new Location(PROVIDER_NAME);
+				loc.setLongitude(Double.parseDouble(buf[0]));
+				loc.setLatitude(Double.parseDouble(buf[1]));
+				loc.setAltitude(Double.parseDouble(buf[2]));
+
+				// 速度を計算して設定
+				long curTime = System.currentTimeMillis();
+				if (m_location != null) {
+					float[] results = new float[3];
+					Location.distanceBetween(this.m_location.getLatitude(), this.m_location.getLongitude(),
+							loc.getLatitude(), loc.getLongitude(), results);
+					float sec = ((float)(curTime - m_location.getTime()))/1000.0f;
+	            	float meterPerSec = results[0] / sec;
+	            	loc.setSpeed(meterPerSec);
+				} else {
+	            	loc.setSpeed(0);
+				}
+            	loc.setTime(curTime);
+
+            	// GPSシグナル発火！
+				this.m_location = loc;
+				this.m_locMan.setTestProviderLocation(PROVIDER_NAME, this.m_location);
+
+				Intent intent = new Intent(CHANGE_LOCATION);
+				intent.putExtra("Latitude", this.m_location.getLatitude());
+				intent.putExtra("Longitude", this.m_location.getLongitude());
+				intent.putExtra("Altitude", this.m_location.getAltitude());
+				intent.putExtra("Speed", this.m_location.getSpeed());
+				intent.putExtra("Time", this.m_location.getTime());
+				GpsSignalService.this.sendBroadcast(intent);
+
+			} catch (IOException e) {
+
+			} catch (NumberFormatException e) {
+
+			} catch (Exception e) {
+
+			}
+
+		}
+
+		// public methods ---------------------------------------------------------
+		// - none -
+
+		// private methods --------------------------------------------------------
+		// - none -
+	}
+
+	class GpsSignalBinder extends Binder {
+
+		GpsSignalService getService() {
+			return GpsSignalService.this;
+		}
+
 	}
 
 }
