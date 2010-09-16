ed989857dbfb51b7d046f8e201f9e3e415e15023
amay077 <okuokuoku@gmail.com>
2010-09-16 14:04:13 +0900

    最初のコミット

---
+++ src/amay077/android/samples/GpsFaker/GpsFaker.java
@@ -0,0 +1,13 @@
+package amay077.android.samples.GpsFaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GpsFaker extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
