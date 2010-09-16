8e8d47575611b416a26cac305788d811da2650ca
amay077 <okuokuoku@gmail.com>
2010-09-16 14:19:47 +0900

    クラス作成

---
+++ src/amay077/android/samples/GpsFaker/GpsSignalService.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package amay077.android.samples.GpsFaker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * @author h_okuyama
+ *
+ */
+public class GpsSignalService extends Service {
+
+	/* (非 Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
